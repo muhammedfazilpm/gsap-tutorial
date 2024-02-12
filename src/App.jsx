54f5566124d77644f020c27a513784f713dbf581
MUHAMMED FAZIL PM <fazilfezz@gmail.com>
2024-02-12 12:41:24 +0530

    fluent ui tested

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Basic from './pages/Basic'
 import Circle from './pages/Circle'
-import Test from './pages/Test'
+import Test from './pages/Windows'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
   <Route path='/' element={<Basic/>}/>
   <Route path='/circle' element={<Circle/>}/>
   <Route path='/test' element={<Test/>}/>
+
   
   </Routes>
    
